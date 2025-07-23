3f05fa01a792aca10a68a1074c4eced54b468dc1
Aaron Rangel <aaron5025@hotmail.com>
2025-07-22 22:54:39 -0500

    Ross feedback

--- library.js
+++ library.js
@@ -82,6 +82,7 @@ module.exports = {
     "populateVariablesOfDataset":handlers.populateVariablesOfDataset,
     "addToJoin":handlers.addToJoin,
     "updateModalHandler": handlers.updateModalHandler,
+    "rconsole_autocompleteHandler":handlers.rconsole_autocompleteHandler,
     "renderChild": handlers.renderChild,
     "renderDependants": handlers.renderDependants,
     "toFocusedInput": handlers.toFocusedInput,
